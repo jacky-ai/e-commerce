dfd13cad2d8b9ea9aabc3bdaaa9c08e838c1a995
pathan jilani <pathanjilani48@gmail.com>
2025-03-09 00:10:03 +0530

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NavComponent } from './nav/nav.component';
+
+export const routes: Routes = [
+    {path:' ',component:WelcomeComponent,title:'homepage'}
+
+    ];
